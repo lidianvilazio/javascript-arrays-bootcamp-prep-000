9c19610ece5c5e96739041ddae31523ecbef0e59
Lidian Vilazio <lidianvilazio@hotmail.com>
2017-12-28 15:35:28 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -3,11 +3,22 @@ var chocolateBars = ["snickers", "hundred", "grand", "kitkat", "skittles"]
 
 function addElementToBeginningOfArray(array, element) {
   return [element, ...array]
-
+//spread operator don't change the array
 }
 
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
   array.unshift(element)
+  return array
+//unshift change the array
+}
+
+function addElementToEndOfArray(array, element) {
+  return [...array, element]
+
+}
+
+function destructivelyAddElementToEndOfArray(array, element) {
+  array.push(element)
   return array
 }
