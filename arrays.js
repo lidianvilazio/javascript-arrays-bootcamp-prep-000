e70ce36c407b92043b22c5233c96b19d7066d039
Lidian Vilazio <lidianvilazio@hotmail.com>
2017-12-28 15:56:47 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -37,8 +37,8 @@ function destructivelyRemoveElementFromBeginningOfArray(array) {
 
 
 function removeElementFromBeginningOfArray(array) {
-  array.slice(0)
-  return array
+  return array.slice(0)
+
 }
 
 
